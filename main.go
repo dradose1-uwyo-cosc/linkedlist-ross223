04b6bad8df5abfba6549661fe3f30f0efa5483da
ross223 <144744308+ross223@users.noreply.github.com>
2026-02-05 19:47:29 -0700

    Update main.go

--- main.go
+++ main.go
@@ -1,56 +1,55 @@
-//[Your Name Here]
+//Ross Anderson
 //COSC 3750
-//[Date Here]
+//2/5/2026
 //
 /*
-	Don't forget to run your go mod init command in your terminal
-	Review the assignment instructions for running your code
-	All the code you need to write should be put in the /ds/ package files
-	Uncomment the import statement for your module name
-	you can uncomment the tests in main as you go to test
-	The code in main is not an extensive test, you should add more and test your code as needed
+        Don't forget to run your go mod init command in your terminal
+        Review the assignment instructions for running your code
+        All the code you need to write should be put in the /ds/ package files
+        Uncomment the import statement for your module name
+        you can uncomment the tests in main as you go to test
+        The code in main is not an extensive test, you should add more and test your code as needed
 */
 package main
 
 import (
-	"fmt"
-	/*"hw01/ds" */ //this needs to be changes to your module name and uncommented
+        "fmt"
+	"hw01/ds"
 )
 
 func main() {
 	fmt.Println("Only here so the import doesn't leave an error")
+        linkedlist := &ds.LinkedList{}
+        linkedlist.InsertAt(0, "first")
+        linkedlist.Insert("first")
+        linkedlist.Insert("first")
+        linkedlist.Insert("second")
+        linkedlist.Insert("third")
+        linkedlist.Insert("fourth")
+        linkedlist.Insert("fifth")
+        linkedlist.RemoveAt(4)
+        linkedlist.PrintList()
+        fmt.Println("The size of the linked list is:", linkedlist.GetSize())
+        fmt.Println("-------------")
+        linkedlist.RemoveAll("first")
+        linkedlist.PrintList()
+        fmt.Println("-------------")
+        linkedlist.Reverse()
+        linkedlist.PrintList()
+        fmt.Println("The size of the linked list is:", linkedlist.GetSize())
+        fmt.Println("-------------")
 
-	//linkedlist := &ds.LinkedList{}
-	//linkedlist.InsertAt(0, "first")
-	//linkedlist.Insert("first")
-	//linkedlist.Insert("first")
-	//linkedlist.Insert("second")
-	//linkedlist.Insert("third")
-	//linkedlist.Insert("fourth")
-	//linkedlist.Insert("fifth")
-	//linkedlist.RemoveAt(4)
-	//linkedlist.PrintList()
-	//fmt.Println("The size of the linked list is:", linkedlist.GetSize())
-	//fmt.Println("-------------")
-	//linkedlist.RemoveAll("first")
-	//linkedlist.PrintList()
-	//fmt.Println("-------------")
-	//linkedlist.Reverse()
-	//linkedlist.PrintList()
-	//fmt.Println("The size of the linked list is:", linkedlist.GetSize())
-	//fmt.Println("-------------")
+        stack := &ds.Stack{}
+        stack.Push("first")
+        stack.Push("second")
+        stack.Push("third")
+        data, _ := stack.Pop()
+        println("Popped from stack:", data)
 
-	//stack := &ds.Stack{}
-	//stack.Push("first")
-	//stack.Push("second")
-	//stack.Push("third")
-	//data, _ := stack.Pop()
-	//println("Popped from stack:", data)
-
-	//queue := &ds.Queue{}
-	//queue.Push("first")
-	//queue.Push("second")
-	//queue.Push("third")
-	//data, _ = queue.Pop()
-	//println("Popped from queue:", data)
+        queue := &ds.Queue{}
+        queue.Push("first")
+        queue.Push("second")
+        queue.Push("third")
+        data, _ = queue.Pop()
+        println("Popped from queue:", data)
 }
